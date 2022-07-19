345d0ae370df786f53dc03a6eefcc86cc237173c
Ada Jasik <jollyzombie@protonmail.com>
2022-07-19 18:31:10 +0200

    Install redux

---
+++ src/components/CardForm/CardForm.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+import styles from './CardForm.module.scss';
+
+const CardForm = (props) => {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.action({ title: title }, props.columnId);
+    setTitle('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.cardForm}>
+      <TextInput type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+      <Button>Add Card</Button>
+    </form>
+  );
+};
+
+export default CardForm;
